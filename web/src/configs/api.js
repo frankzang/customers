64657ef0f3a7e878b7c10bdbafd860fe0826fa94
Francisco Santos <fsantos.fs37@gmail.com>
2020-08-15 16:40:54 -0300

    remove logs

--- web/src/configs/api.js
+++ web/src/configs/api.js
@@ -2,7 +2,6 @@ const api = "http://localhost:5000/";
 
 function requestApi(endpoint, { method = "GET" } = {}) {
   const path = `${api}${endpoint}`;
-  console.log(path);
   return fetch(path, {
     method,
   });
