d5c24a8050d77777a83b1a280cc67a4c2b0122e1
Francisco Santos <fsantos.fs37@gmail.com>
2020-08-15 20:09:58 -0300

    add button to screen

--- web/src/screens/Login/index.jsx
+++ web/src/screens/Login/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { LoginButton } from "./LoginButton";
+import { Button } from "../../components/Button";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
 import { Routes } from "../../configs/routes";
 
 export default function Login() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, loginWithPopup } = useAuth0();
   const history = useHistory();
 
   if (isAuthenticated) {
@@ -14,7 +14,7 @@ export default function Login() {
 
   return (
     <section>
-      <LoginButton />
+      <Button onClick={loginWithPopup} />
     </section>
   );
 }
