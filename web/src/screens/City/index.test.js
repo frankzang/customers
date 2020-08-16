cef621dca4b8e3c53dc09b47d33e8336b8411b64
Francisco Santos <fsantos.fs37@gmail.com>
2020-08-15 22:33:26 -0300

    add test to city page

---
+++ web/src/screens/City/index.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  render,
+  screen,
+  waitForDomChange,
+  fireEvent,
+  act,
+} from "@testing-library/react";
+import City from "./";
+import { buildCustomer } from "../../tests/generate";
+import { MemoryRouter } from "react-router-dom";
+
+describe("City component", () => {
+  test("should fetch a new set of results", async () => {
+    const page1 = Array(10)
+      .fill(null)
+      .map(() => buildCustomer());
+    const page2 = Array(5)
+      .fill(null)
+      .map(() => buildCustomer());
+    jest.spyOn(global, "fetch").mockReturnValue({
+      json: () => Promise.resolve({ customers: page1, hasMore: true }),
+      status: 200,
+    });
+    render(<City />, { wrapper: MemoryRouter });
+
+    await waitForDomChange();
+
+    expect(screen.getAllByRole("link").length).toBe(page1.length);
+    expect(
+      screen.getByRole("button", {
+        name: /previous page/i,
+      })
+    ).toBeDisabled();
+
+    jest.spyOn(global, "fetch").mockReturnValue({
+      json: () => Promise.resolve({ customers: page2, hasMore: false }),
+      status: 200,
+    });
+
+    await act(async () => {
+      fireEvent.click(
+        screen.getByRole("button", {
+          name: /next page/i,
+        })
+      );
+    });
+
+    expect(screen.getAllByRole("link").length).toBe(page2.length);
+    expect(
+      screen.getByRole("button", {
+        name: /next page/i,
+      })
+    ).toBeDisabled();
+  });
+});
